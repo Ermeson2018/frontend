d27916c7973e5b9ac906e494e2733797688c2b1d
Ermeson Oliveira <ermesonoliveira2000@gmail.com>
2019-06-25 23:49:28 -0300

    Updates

---
+++ precache-manifest.7ef7e6a2044a4745b7c264455e90cc62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c532ab1147de590e4ed2cba8cef6b0c9",
+    "url": "/Ermeson2018/frontend/index.html"
+  },
+  {
+    "revision": "2cf967054f86899987cc",
+    "url": "/Ermeson2018/frontend/static/css/main.a6f91605.chunk.css"
+  },
+  {
+    "revision": "e747898a2953d4b5ebde",
+    "url": "/Ermeson2018/frontend/static/js/2.54859327.chunk.js"
+  },
+  {
+    "revision": "2cf967054f86899987cc",
+    "url": "/Ermeson2018/frontend/static/js/main.6f79c7e5.chunk.js"
+  },
+  {
+    "revision": "6c69e97ded394db00062",
+    "url": "/Ermeson2018/frontend/static/js/runtime~main.f2bab168.js"
+  },
+  {
+    "revision": "87f186624f5a5e1b954a172ef0e969a1",
+    "url": "/Ermeson2018/frontend/static/media/logo.87f18662.svg"
+  }
+]);
